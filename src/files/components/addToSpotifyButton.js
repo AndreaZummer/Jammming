e81965e181ed6587cd13787c543fd03361f4115e
Andrea Zummerova <adkabodiova@gmail.com>
2025-01-07 11:14:17 +0100

    static button

---
+++ src/files/components/addToSpotifyButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function addToSpotifyButton() {
+    return 
+        <button>Add Playlist To Spotify</button>
+};
+
+export default addToSpotifyButton;
---
+++ src/files/components/searchButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function SearchButton() {
+    return 
+        <button> Search </button>
+};
+
+export default SearchButton;
