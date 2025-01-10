6501aba4976ca822bcc5831defe2a4b482716fd3
Andrea Zummerova <adkabodiova@gmail.com>
2025-01-10 09:15:15 +0100

    accept playlistName

--- src/files/components/playlist.js
+++ src/files/components/playlist.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Tracklist from '../containers/tracklist';
 
 function PLaylist(props) {
-    console.log(props.selected);
+
+    const [name, setName]=useState('');
+    const [playlistName, setPlaylistName]=useState('');
+
+    function namingPlaylist(event) {
+        setName(event.target.value);
+    };
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setPlaylistName(name);
+        setName('');
+    };
+
     return (
             <div>
-                <input type="text"  placeholder="New Playlist" onChange />
+                <form onSubmit={handleSubmit}>
+                    <input type="text"  placeholder="New Playlist" onChange={namingPlaylist} value={name}/>
+                </form>
+                <h2>{playlistName}</h2>
                 <Tracklist tracklist={props.selected} removeClick={props.removeClick} />
             </div>
     )
