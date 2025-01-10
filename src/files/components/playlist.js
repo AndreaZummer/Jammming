1aabdc1e2884fd49175d3d42f9f72681a74d3b98
Andrea Zummerova <adkabodiova@gmail.com>
2025-01-10 09:43:03 +0100

    changing name

--- src/files/components/playlist.js
+++ src/files/components/playlist.js
@@ -5,6 +5,7 @@ function PLaylist(props) {
 
     const [name, setName]=useState('');
     const [playlistName, setPlaylistName]=useState('');
+    const [disabled, setDisabled] = useState(false);
 
     function namingPlaylist(event) {
         setName(event.target.value);
@@ -13,15 +14,25 @@ function PLaylist(props) {
     function handleSubmit(event) {
         event.preventDefault();
         setPlaylistName(name);
-        setName('');
+        setName('Already Named');
+        setDisabled(true);
+    };
+
+    function handleChangingNameClick(event) {
+      setName(playlistName);
+      setPlaylistName('');
+      setDisabled(false);  
     };
 
     return (
             <div>
                 <form onSubmit={handleSubmit}>
-                    <input type="text"  placeholder="New Playlist" onChange={namingPlaylist} value={name}/>
+                    <input type="text"  placeholder="New Playlist" onChange={namingPlaylist} value={name} disabled={disabled}/>
                 </form>
                 <h2>{playlistName}</h2>
+                {playlistName && 
+                    <button onClick={handleChangingNameClick}> Change Playlist Name</button>}
+
                 <Tracklist tracklist={props.selected} removeClick={props.removeClick} />
             </div>
     )
