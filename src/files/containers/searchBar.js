383d42e8334564d55e19c575e9b6d866a61342ce
Andrea Zummerova <adkabodiova@gmail.com>
2025-01-13 09:42:08 +0100

    with searchResults

--- src/files/containers/searchBar.js
+++ src/files/containers/searchBar.js
@@ -1,20 +1,53 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import SearchButton from '../components/searchButton';
+import SearchResults from './searchResults';
+import AddToSpotifyButton from '../components/addToSpotifyButton';
+import PLaylist from '../components/playlist';
+import { getSearchResults} from '../utilities/utilities';
 
 function SearchBar(props) {
 
     const [search, setSearch] = useState('');
+    const [searchResults, setSearchResults] = useState(null);
+    const [selected, setSelected] = useState([]);
+                  
+    function handleSelectionClick(newSelectedTrack) {
+        setSelected([...selected, newSelectedTrack])
+    };
+      
+    function handleRemoveClick(removedTrack) {
+        setSelected((selected) => {return selected.filter(track => track!==removedTrack)})
+    };
 
     function handleChange(event) {
-        setSearch(event.target.value)
+        setSearch(event.target.value);
     };
+
+    async function handleSearchClick() {
+        setSearchResults(await getSearchResults(search));
+        setSearch('');
+        props.handleSearching();
+    };
+
+    useEffect(() => {
+        console.log('Updated searchResults:', searchResults);
+    }, [searchResults]);
 
     return (
         <div>
-            <input type="text" placeholder='Song Title / Artist' value={search} onChange={handleChange}/>
-            <SearchButton />
+            <div>
+                <input type="text" placeholder='Song Title' value={search} onChange={handleChange}/>
+                <SearchButton onClick=/*{() => */{handleSearchClick/*; manageStatesOnClick();}*/}/>
+            </div>
+            {props.searching && (
+            <div>
+                <SearchResults searchResults={searchResults} selectionClick={handleSelectionClick}/>
+                <PLaylist removeClick={handleRemoveClick} selected={selected}/>
+                <AddToSpotifyButton />
+            </div>
+            )}
         </div>
     )    
 };
-// export {search};
+
 export default SearchBar;
