ead58c2ea650c88684796add10da7e0e95dcaca6
Andrea Zummerova <adkabodiova@gmail.com>
2025-02-20 22:34:39 +0100

    no data handling

--- src/files/components/searchResults.js
+++ src/files/components/searchResults.js
@@ -5,12 +5,16 @@ function SearchResults(props) {
     return (
         <div className='searchResults'>
             <h2>Search Results</h2>
-            {props.searchResults.map((track,index) => { return (
-                <Track 
-                    track={track} 
-                    key={`track${index}`} 
-                    selectionClick={props.selectionClick}/>
-            )})}
+            {props.searchResults? 
+                props.searchResults.map((track,index) => { return (
+                    <Track 
+                        track={track} 
+                        key={`track${index}`} 
+                        selectionClick={props.selectionClick}/>
+                )}) 
+                : 
+                <p style={{fontSize: '1.35rem', padding: '1rem 2rem', }}> No available results...</p>
+            }
         </div>
     )
 };
